181c809df07d23d47006e59b4dad7ddb8cd050ca
Mac Siri <krairit.siri@gmail.com>
2016-10-28 16:31:47 -0400

    implement abstract tile classes

---
+++ src/chess/Tile.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chess;
+
+/**
+ *
+ * @author Mac
+ */
+public abstract class Tile {
+    
+    int tileCoord;
+    
+    Tile(int tileCoord){
+        this.tileCoord = tileCoord;
+    }
+    
+    public abstract boolean isTileOcupied();
+    
+    public abstract Piece getPiece();
+    
+    public static final class EmptyTile extends Tile {
+        
+        EmptyTile(int coord) {
+            super(coord);
+        }
+        
+        @Override
+        public boolean isTileOccupied() {
+            return false;
+        }
+        
+        @Override        
+        public Piece getPiece() {
+            return null;
+        }
+    }
+    
+    public static final class OccupiedTile extends Tile {
+        
+        Piece pieceOnTile;
+        
+        OccupiedTile(int tileCoord, Piece piece) {
+            super(tileCoord);
+            this.pieceOnTile = pieceOnTile;
+        }
+    }
+    
+    
+}
